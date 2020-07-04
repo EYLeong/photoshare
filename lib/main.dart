5f2ccc23eddd58c384760890612867b41f33a9c2
EYLeong <leong.enyi.1997@gmail.com>
2020-07-04 15:23:45 +0800

    learning about state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,11 +7,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+      home: HomeScreenWidget(),
+    );
+  }
+}
+
+class HomeScreenWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Hello World!"),
         centerTitle: true,
       ),
-    ));
+      body: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _count = 0;
+
+  void _incrementCount() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('$_count'),
+        RaisedButton(onPressed: _incrementCount),
+      ],
+    );
   }
 }
