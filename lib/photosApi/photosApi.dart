79efc7721ba2cb543a500fe58b6b893f1a87ec93
EYLeong <leong.enyi.1997@gmail.com>
2020-07-10 17:02:22 +0800

    edit

--- lib/photosApi/photosApi.dart
+++ lib/photosApi/photosApi.dart
@@ -6,15 +6,26 @@ import 'package:http/http.dart';
 
 import 'album.dart';
 
-Future<List<Album>> listAlbums(Future<Map<String, String>> authHeaders) async {
-  List<Album> albumList = [];
+///Returns a list of Albums from the Google Photos API.
+Future<List<Album>> listAlbums(Future<Map<String, String>> authHeaders,
+    {int pageSize = 20, String pageToken = ''}) async {
+  final List<Album> albumList = [];
+  final String url =
+      'https://photoslibrary.googleapis.com/v1/albums?pageSize=$pageSize&pageToken=$pageToken&excludeNonAppCreatedData=false';
 
-  const String url = 'https://photoslibrary.googleapis.com/v1/albums';
+  final Response res = await http.get(url, headers: await authHeaders);
+  final Map<String, dynamic> resJson = jsonDecode(res.body);
 
-  Response res = await http.get(url, headers: await authHeaders);
-  jsonDecode(res.body)["albums"].forEach((albumJson) {
-    albumList.add(Album.fromJson(albumJson));
-  });
+  if (resJson["error"] != null) {
+    if (resJson["error"]["status"] == "INVALID_ARGUMENT")
+      throw FormatException(resJson["error"]["message"]);
+    throw Exception(resJson["error"]["message"]);
+  }
 
+  if (resJson["albums"] != null) {
+    resJson["albums"].forEach((albumJson) {
+      albumList.add(Album.fromJson(albumJson));
+    });
+  }
   return albumList;
 }
